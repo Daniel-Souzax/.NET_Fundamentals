e8df6252bd2b9336cbb2996919db17d778bfbf03
Daniel S. Filho <danieldsfilho.dev@gmail.com>
2024-04-22 14:55:12 -0300

    feat: module other types of data

---
+++ MyProject/Fundamentals/Fundamentals/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Fundamentals;
+
+class Program
+{   
+
+    static void Main()
+    {
+
+        int? idade = null;
+        
+        int minhaIdade = idade.Value;
+
+        Console.WriteLine(minhaIdade);
+        
+
+    }
+}
