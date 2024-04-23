4d2937b2bbf6abe2d56758bec84dfd962896743f
Daniel S. Filho <danieldsfilho.dev@gmail.com>
2024-04-23 16:15:13 -0300

    fetch: module colection

---
+++ MyProject/Fundamentals/Fundamentals/OperacoesMatematicas.cs
@@ -0,0 +1,19 @@
+﻿namespace Fundamentals;
+
+public class OperacoesMatematicas
+{
+    public (int resultadoDaAdicao, string autor) Adicionar(int valor1, int valor2)
+    {
+        var resultado = valor1 + valor2;
+
+        return (resultado, "dan");
+
+    } 
+
+
+    public void Teste(int valor1, int valor2 = 7)
+    {
+        Console.WriteLine(valor1 + valor2);
+    }
+
+}
