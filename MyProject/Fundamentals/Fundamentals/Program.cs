dc2f5436793086d2026fb471fb9cb5b889a2b57a
Daniel S. Filho <danieldsfilho.dev@gmail.com>
2024-04-25 16:02:52 -0300

    feat: added loop

--- MyProject/Fundamentals/Fundamentals/Program.cs
+++ MyProject/Fundamentals/Fundamentals/Program.cs
@@ -1,57 +1,22 @@
 ﻿namespace Fundamentals;
 
 class Program
-{  
-      
-    enum Cores
-    {
-        Verde, 
-        Preto,
-        Laranja,
-        Lilas
-    }
+{
+
     static void Main()
     {
-        string autor = "Dan";
-        Cores cor = Cores.Preto;
-        int numero = 1;
+        var numero = 10;
 
-        string resultado = numero switch
+        do
         {
-            7 => "Dan",
-            1 => "Sara",
-            3 => "lele",
-            _ => "Nome desconhecido"
-        };
+            Console.WriteLine("Teste");
 
-        Console.WriteLine(resultado);
-
-        //Switch normal
-        /*
-        switch (cor)
-        {
-            case Cores.Laranja:
-                {
-                    Console.WriteLine("Laranja");
-                }
-                break;
-            
-            case Cores.Lilas:
-                {
-                    Console.WriteLine("Lilas");
-                }
-                break;
-            
-            default:
-                {
-                    Console.WriteLine("nenhuma das anteriores");
-                }
-                break;
-            
+            numero++;
         }
-        */
+        while (numero < 10);
+        
 
+        Console.WriteLine("Saiu");
     }
-
 }
 
